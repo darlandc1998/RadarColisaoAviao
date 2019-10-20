6c12973ee958ac1db062479a414fa05c3d3906a6
darlan <darlandc1998>
2019-10-20 12:12:50 -0300

    Segundo commit. #master

--- src/main/java/com/radar/Main.java
+++ src/main/java/com/radar/Main.java
@@ -1,10 +1,9 @@
 package com.radar;
 
-
 public class Main {
     
     public static void main(String args[]){
-        System.out.println("Inicio...");
+        System.out.println("Inicio....");        
     }
     
 }
